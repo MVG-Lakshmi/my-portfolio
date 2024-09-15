0d8e565492ab85bff6a92f4c7fa36a8a48bcb673
Motamarri Venkata Gangadha Lakshmi <mvglakshmi3@gmail.com>
2024-09-15 23:55:04 +0530

    Initial commit

---
+++ src/Utils/data.js
@@ -0,0 +1,76 @@
+export const skills = [
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+        title: "HTML",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+        title: "CSS",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "backend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
+        title: "react",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/fluency/512/node-js.png",
+        title: "Node.js",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/mongodb.png",
+        title: "MongoDB",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/express-js.png",
+        title: "Express.js",
+        tag: "backend"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/bootstrap.png",
+        title: "Bootstrap",
+        tag: "frontend"
+    },
+    {
+        icon: "https://img.icons8.com/ios-glyphs/512/github.png",
+        title: "Github",
+        tag: "platform"
+    },
+    {
+        icon: "https://i.ibb.co/61nck0S/download-removebg-preview.png",
+        title: "VS Code",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/firebase.png",
+        title: "Firebase",
+        tag: "platform"
+    }
+
+]
+
+export const projects = [
+    {
+        id: 0,
+        title: 'Selection of Organic Food Farming by Analytic Hierarchy Process (AHP)',
+        type: 'Operations-Research',
+        image: 'https://miro.medium.com/v2/resize:fit:800/1*dqFqPTUB7bn3am5U7uzBEQ.jpeg',
+        description: 'The main objective of this project is selection of organic food that has to be grown on the agricultural land particularly using AHP Methodology.',
+        techStack: [],
+        github: '',
+        liveDemo: ''
+    }
+]
